e1c0baa862a1bb5e00a6e5ad649c04e9a8368df3
onedayRen <onedayRen@163.com>
2018-07-05 14:34:05 +0800

    BookBean

---
+++ src/main/java/com/yitian/controller/domain/BookBean.java
@@ -0,0 +1,33 @@
+package com.yitian.controller.domain;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "book",locations = "classpath:book.properties")
+public class BookBean {
+	private String name;
+    private String author;
+    private String price;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+    public String sleep(){
+    	return "睡觉";
+    }
+}
